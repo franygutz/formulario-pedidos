2252d6b0e1f825b74b8ccb535b8d206dc07ca8ff
franygutz <franyagl@uamv.edu.ni>
2026-04-14 15:10:06 -0600

    Error reparado

--- app/src/main/java/com/example/app_pedidos/MainActivity.kt
+++ app/src/main/java/com/example/app_pedidos/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            //quitar el innerpadding
             App_PedidosTheme {
                 FormularioScreen()
             }
